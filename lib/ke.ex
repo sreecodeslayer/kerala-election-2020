d2b2696626ce2cca313ca796b927a66eb2f54f13
Sreenadh TC <kesav.tc8@gmail.com>
2020-12-16 19:56:50 +0530

    add functions to parse each local bodies

---
+++ lib/ke.ex
@@ -0,0 +1,308 @@
+defmodule Ke do
+  @base_url "https://results.prdlive.trtech.in/json/"
+
+  require Logger
+
+  def start() do
+    dists = get_districts()
+
+    Enum.map(dists, fn d ->
+      district = get_full_district(d)
+      Logger.info(String.duplicate("==", 25))
+    end)
+  end
+
+  def get_full_district(d, save? \\ true) do
+    Logger.debug("Starting to parse district #{d}")
+    all_gps = get_gram_panchs_for_dist(d)
+    :timer.sleep(1000)
+    all_blocks = get_block_panchs_for_dist(d)
+    :timer.sleep(1000)
+    all_dt_panchs = get_dist_panchs_for_dist(d)
+    :timer.sleep(1000)
+    all_urbans = get_urbans_for_dist(d)
+
+    all_gps =
+      Enum.map(all_gps, fn gp ->
+        gp = get_gram_panch(gp)
+        :timer.sleep(1000)
+        gp
+      end)
+
+    Logger.info(String.duplicate("--", 25))
+
+    all_blocks =
+      Enum.map(all_blocks, fn bl ->
+        bl = get_block_panch(bl)
+        :timer.sleep(1000)
+        bl
+      end)
+
+    Logger.info(String.duplicate("--", 25))
+
+    all_dt_panchs =
+      Enum.map(all_dt_panchs, fn dpn ->
+        dpn = get_district_panch(dpn)
+        :timer.sleep(1000)
+        dpn
+      end)
+
+    Logger.info(String.duplicate("--", 25))
+
+    all_urbans =
+      Enum.map(all_urbans, fn ubn ->
+        ubn = get_urban_body(ubn)
+        :timer.sleep(1000)
+        ubn
+      end)
+
+    d = %{
+      dist_code: d,
+      gram_panchayats: all_gps,
+      block_panchayats: all_blocks,
+      district_panchayats: all_dt_panchs,
+      urban_bodies: all_urbans
+    }
+
+    if save?, do: File.write("#{d}_data.json", Jason.encode!(d)), else: d
+  end
+
+  def get_districts do
+    ds = HTTPoison.get!(@base_url <> "sv2lpgl.json?_=#{ts()}", headers())
+    json_data = Jason.decode!(ds.body)
+
+    Enum.map(json_data["total"], fn [dist | _] -> dist end)
+  end
+
+  def get_gram_panchs_for_dist(dist_code) do
+    Logger.debug("Fetching gram_panchayats for dist_code: #{dist_code}")
+    url = @base_url <> "dvGramaLead_#{dist_code}.json?_=#{ts()}"
+    dist = HTTPoison.get!(url, headers())
+    json_data = Jason.decode!(dist.body)
+
+    Enum.map(json_data["payload"], fn [gp_code, gp_name | _] ->
+      %{code: gp_code, name: String.upcase(gp_name)}
+    end)
+  end
+
+  def get_block_panchs_for_dist(dist_code) do
+    Logger.debug("Fetching block_panchayats for dist_code: #{dist_code}")
+    url = @base_url <> "dvBlockLead_#{dist_code}.json?_=#{ts()}"
+    dist = HTTPoison.get!(url, headers())
+    json_data = Jason.decode!(dist.body)
+
+    Enum.map(json_data["payload"], fn [block_code, block_name | _] ->
+      %{code: block_code, name: String.upcase(block_name)}
+    end)
+  end
+
+  def get_dist_panchs_for_dist(dist_code) do
+    Logger.debug("Fetching district_panchayats for dist_code: #{dist_code}")
+    url = @base_url <> "dvDistLead_#{dist_code}.json?_=#{ts()}"
+    dist = HTTPoison.get!(url, headers())
+    json_data = Jason.decode!(dist.body)
+
+    Enum.map(json_data["payload"], fn [dist_code, dist_name | _] ->
+      %{code: dist_code, name: String.upcase(dist_name)}
+    end)
+  end
+
+  def get_urbans_for_dist(dist_code) do
+    Logger.debug("Fetching urban bodies for dist_code: #{dist_code}")
+    url = @base_url <> "dvUrbanLead_#{dist_code}.json?_=#{ts()}"
+    dist = HTTPoison.get!(url, headers())
+    json_data = Jason.decode!(dist.body)
+
+    Enum.map(json_data["payload"], fn [dist_code, dist_name | _] ->
+      %{code: dist_code, name: String.upcase(dist_name)}
+    end)
+  end
+
+  def get_gram_panch(%{code: gp_code, name: name}) do
+    Logger.debug("Fetching ward data for: #{gp_code} - #{name}")
+    url = @base_url <> "#{gp_code}_L.json?_=#{ts()}"
+    gp = HTTPoison.get!(url, headers())
+
+    json_data = Jason.decode!(gp.body)
+
+    data = Enum.map(json_data["payload"], &parse_local_body_data(&1))
+
+    %{body: name, code: gp_code, data: data}
+  end
+
+  def get_block_panch(%{code: bl_code, name: name}) do
+    Logger.debug("Fetching block data for: #{bl_code} - #{name}")
+    url = @base_url <> "#{bl_code}_L.json?_=#{ts()}"
+
+    block = HTTPoison.get!(url, headers())
+
+    unless block.status_code == 403 do
+      json_data = Jason.decode!(block.body)
+
+      data = Enum.map(json_data["payload"], &parse_local_body_data(&1))
+
+      %{body: name, code: bl_code, data: data}
+    else
+      Logger.error("Could not fetch due to 403 status code for #{name}")
+      %{body: name, code: bl_code, data: "Could not fetch due to 403 status code"}
+    end
+  end
+
+  def get_urban_body(%{code: ubn_code, name: name}) do
+    Logger.debug("Fetching urban data for: #{ubn_code} - #{name}")
+    url = @base_url <> "#{ubn_code}_L.json?_=#{ts()}"
+
+    block = HTTPoison.get!(url, headers())
+
+    unless block.status_code == 403 do
+      json_data = Jason.decode!(block.body)
+
+      data =
+        Enum.map(json_data["payload"], fn json ->
+          # Leading candidate
+          w_l_party = Enum.at(json, 1)
+
+          w_l_cn_num = Enum.at(json, 2)
+          w_l_cn_name = Enum.at(String.split(Enum.at(json, 3), "-", trim: true), 1)
+
+          w_l_cn_votes = String.to_integer(Enum.at(json, 4))
+
+          # Trailing candidate
+          place_name = Enum.at(json, 5)
+
+          w_t_cn_num = Enum.at(json, 7)
+          w_t_cn_name = Enum.at(String.split(Enum.at(json, 8), "-", trim: true), 1)
+
+          w_t_cn_votes = String.to_integer(Enum.at(json, 9))
+
+          %{
+            lead: %{
+              num: w_l_cn_num,
+              name: String.trim(w_l_cn_name),
+              votes: w_l_cn_votes,
+              party: w_l_party
+            },
+            trail: %{
+              num: w_t_cn_num,
+              name: String.trim(w_t_cn_name),
+              votes: w_t_cn_votes
+            },
+            place: place_name
+          }
+        end)
+
+      %{body: name, code: ubn_code, data: data}
+    else
+      Logger.error("Could not fetch due to 403 status code for #{name}")
+      %{body: name, code: ubn_code, data: "Could not fetch due to 403 status code"}
+    end
+  end
+
+  def get_district_panch(%{code: d_code, name: name}) do
+    Logger.debug("Fetching dist panchayat data for: #{d_code} - #{name}")
+    url = @base_url <> "#{d_code}_L.json?_=#{ts()}"
+    gp = HTTPoison.get!(url, headers())
+
+    json_data = Jason.decode!(gp.body)
+
+    data =
+      Enum.map(json_data["payload"], fn json ->
+        # Leading candidate
+        w_l_party = Enum.at(json, 1)
+
+        w_l_cn_num = Enum.at(json, 2)
+        w_l_cn_name = Enum.at(String.split(Enum.at(json, 3), "-", trim: true), 1)
+
+        w_l_cn_votes = String.to_integer(Enum.at(json, 4))
+
+        # Trailing candidate
+        place_name = Enum.at(json, 5)
+
+        w_t_cn_num = Enum.at(json, 7)
+        w_t_cn_name = Enum.at(String.split(Enum.at(json, 8), "-", trim: true), 1)
+
+        w_t_cn_votes = String.to_integer(Enum.at(json, 9))
+
+        %{
+          lead: %{
+            num: w_l_cn_num,
+            name: String.trim(w_l_cn_name),
+            votes: w_l_cn_votes,
+            party: w_l_party
+          },
+          trail: %{
+            num: w_t_cn_num,
+            name: String.trim(w_t_cn_name),
+            votes: w_t_cn_votes
+          },
+          place: place_name
+        }
+      end)
+
+    %{body: name, code: d_code, data: data}
+  end
+
+  defp parse_local_body_data(json) do
+    # Leading candidate
+    w_l_party = Enum.at(json, 1)
+
+    w_l_cn_num = Enum.at(json, 2)
+    w_l_cn_name = Enum.at(String.split(Enum.at(json, 3), "-", trim: true), 1)
+
+    w_l_cn_votes = String.to_integer(Enum.at(json, 4))
+
+    # Trailing candidate
+    w_t_party = Enum.at(json, 5)
+
+    w_t_cn_num = Enum.at(json, 7)
+    w_t_cn_name = Enum.at(String.split(Enum.at(json, 8), "-", trim: true), 1)
+
+    w_t_cn_votes = String.to_integer(Enum.at(json, 9))
+
+    %{
+      lead: %{
+        num: w_l_cn_num,
+        name: String.trim(w_l_cn_name),
+        votes: w_l_cn_votes,
+        party: w_l_party
+      },
+      trail: %{
+        num: w_t_cn_num,
+        name: String.trim(w_t_cn_name),
+        votes: w_t_cn_votes,
+        party: w_t_party
+      }
+    }
+  end
+
+  defp headers do
+    [
+      {"User-Agent", Enum.random(uas())},
+      {"Host", "results.prdlive.trtech.in"},
+      {"Accept", "application/json"},
+      {"Referer", "https://results.prdlive.trtech.in/index.html"},
+      {"Connection", "keep-alive"},
+      {"X-Requested-With", "XMLHttpRequest"},
+      {"Pragma", "no-cache"},
+      {"Accept-Language", "en-US,en;q=0.5"},
+      {"Accept-Encoding", "gzip, deflate, br"},
+      {"Cache-Control", "no-cache"}
+    ]
+  end
+
+  defp uas do
+    [
+      "Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
+      "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36",
+      "Mozilla/5.0 (Linux; Android 7.0; SM-G930VC Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36",
+      "Mozilla/5.0 (Linux; Android 6.0.1; SM-G935S Build/MMB29K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36",
+      "Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36",
+      "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 6P Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36",
+      "Mozilla/5.0 (Linux; Android 6.0.1; E6653 Build/32.2.A.0.253) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36"
+    ]
+  end
+
+  defp ts do
+    DateTime.to_unix(DateTime.utc_now())
+  end
+end
